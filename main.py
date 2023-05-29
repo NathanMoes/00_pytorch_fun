dd304650052dd6b955f8ac80b4cd3e5e9e184f8a
Nathan Moes <nathanmoes7@gmail.com>
2023-05-29 13:00:34 -0700

    added device angnoistic

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
+device = "cpu"
+
+if torch.cuda.is_available():
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+    device = 'cuda'
 
 Tensor = torch.rand(size=(3, 4))
 Tensor += 10

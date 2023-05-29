4b64aafbdf810c47d14dd68b504ef89bd7cf910a
Nathan Moes <nathanmoes7@gmail.com>
2023-05-29 10:14:31 -0700

    enable tensor cores

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-Tensor = torch.rand(3, 4)
+torch.backends.cuda.matmul.allow_tf32 = True
+torch.backends.cudnn.allow_tf32 = True
+
+Tensor = torch.rand(size=(3, 4))
+Tensor += 10
 print(Tensor)

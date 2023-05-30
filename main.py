039213faefbbb3540fb8baa1c0411381e43629b5
Nathan Moes <nathanmoes7@gmail.com>
2023-05-30 11:53:06 -0700

    print out loss curves

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ torch.device(device=device)
 def pytorchWorkFlow():
 
     class LinearRegModel(nn.Module):
-        def __init__(self, X_Train, y_train, *args, **kwargs) -> None:
+        def __init__(self, X_Train, y_train, X_test, y_test, *args, **kwargs) -> None:
             super().__init__(*args, **kwargs)
             self.weights = nn.Parameter(torch.randn(
                 1, requires_grad=True, dtype=torch.float32))
@@ -25,22 +25,56 @@ def pytorchWorkFlow():
                 1, requires_grad=True, dtype=torch.float32))
             self.loss_fn = nn.L1Loss()
             self.optimizer = torch.optim.SGD(
-                params=self.parameters(), lr=0.01)
-            self.epochs = 1000
+                params=self.parameters(), lr=0.001)
+            self.epochs = 1700
             self.X_train = X_train
+            self.X_test = X_test
+            self.y_test = y_test
+            self.epoch_count = []
+            self.loss_values = []
+            self.test_loss_values = []
 
         def forward(self, x: torch.Tensor) -> torch.Tensor:
             return self.weights * x + self.bias
 
+        def test_output(self, epoch, loss):
+            with torch.inference_mode():  # trun off tracking of un needed things
+                test_prediction = self.forward(self.X_test)
+                test_loss = self.loss_fn(test_prediction, y_test)
+            if epoch % 10 == 0:
+                print(
+                    f"Epoch: {epoch} | loss: {loss} | Test loss: {test_loss}")
+                print(f"Weight: {self.weights} | bias: {self.bias}")
+            return test_loss
+
+        def print_loss_curve(self):
+            with torch.inference_mode():
+                plt.plot(self.epoch_count, self.loss_values,
+                         label="Train loss")
+                plt.plot(self.epoch_count, self.test_loss_values,
+                         label="Test loss")
+                plt.title('Training and test loss curves')
+                plt.ylabel("Loss")
+                plt.xlabel("Epochs")
+                plt.legend()
+
         def train_loop(self):
+
             for epoch in range(self.epochs):
-                self.train()
-                y_pred = self.forward(self.X_train)
-                loss = self.loss_fn(y_pred, y_train)
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                self.eval()
+                self.epoch_count.append(epoch)
+                self.train()  # turn on grads for needed updates
+                y_pred = self.forward(self.X_train)  # forward pass
+                loss = self.loss_fn(y_pred, y_train)  # calc loss
+                self.loss_values.append(loss)
+                self.optimizer.zero_grad()  # optimse base on gradient?
+                loss.backward()  # back prop error
+                self.optimizer.step()  # perform grad desc
+                self.eval()  # enable testing
+                test_loss = self.test_output(epoch=epoch, loss=loss)
+                self.test_loss_values.append(test_loss)
+                # if epoch % 1000 == 0:
+                #     print(
+                #         f"Loss: {loss}, weight:{self.weights} and bias: {self.bias}")
 
     weight = 0.7
     bias = 0.3
@@ -54,8 +88,10 @@ def pytorchWorkFlow():
     X_train, y_train = X[:train_split], y[:train_split]
     X_test, y_test = X[train_split:], y[train_split:]
     torch.manual_seed(42)
-    my_model = LinearRegModel(X_train, y_train=y_train)
+    my_model = LinearRegModel(X_train, y_train=y_train,
+                              X_test=X_test, y_test=y_test)
     my_model.train_loop()
+    my_model.print_loss_curve()
 
     def plot_Predictions(train_data=X_train, train_label=y_train, test_data=X_test, test_labels=y_test, predictions=None):
         print('e')

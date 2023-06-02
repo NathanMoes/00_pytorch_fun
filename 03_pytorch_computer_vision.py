eb0eccbd35eadcb1ba57a6ef05b3b1960bfa85e5
Nathan Moes <nathanmoes7@gmail.com>
2023-06-02 16:18:23 -0700

    init dataloader

--- 03_pytorch_computer_vision.py
+++ 03_pytorch_computer_vision.py
@@ -8,6 +8,7 @@ from torchvision import datasets
 from torchvision import transforms
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
 
 
 if Path("helper_functions.py").is_file():
@@ -48,6 +49,37 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor(),  # transform into tensor
     target_transform=None  # dont transform the labels
 )
+
+# set batch size
+BATCH_SIZE = 32
+
+# turn dataset into batches
+train_dataloader = DataLoader(
+    dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+test_dataloader = DataLoader(
+    dataset=train_data, batch_size=BATCH_SIZE, shuffle=False)
+
+
+class_names = train_data.classes
+# visualize data
+image, label = train_data[0]
+plt.imshow(image.squeeze(), cmap="gray")
+plt.title(label=class_names[label])
+plt.show()
+torch.manual_seed(42)
+fig = plt.figure(figsize=(9, 9))
+rows, cols = 4, 4
+for i in range(1, rows * cols + 1):
+    random_idx = torch.randint(0, len(train_data), size=[1]).item()
+    img, label = train_data[random_idx]
+    fig.add_subplot(rows, cols, i)
+    plt.imshow(img.squeeze(), cmap="gray")
+    plt.title(class_names[label])
+    plt.axis(False)
+plt.show()
+
+# prepare dataloader
 
 if __name__ == "__main__":
     print('e')

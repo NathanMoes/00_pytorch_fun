9cc3e27390ffb712394e048ecfb6c3c28936c3d2
Nathan Moes <nathanmoes7@gmail.com>
2023-06-02 19:23:05 -0700

    I forgot what I added \(0_0)/

--- 03_pytorch_computer_vision.py
+++ 03_pytorch_computer_vision.py
@@ -9,6 +9,9 @@ from torchvision import transforms
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
+import timeit
+from timeit import default_timer as timer
+from tqdm.auto import tqdm
 
 
 if Path("helper_functions.py").is_file():
@@ -60,7 +63,8 @@ train_dataloader = DataLoader(
 test_dataloader = DataLoader(
     dataset=train_data, batch_size=BATCH_SIZE, shuffle=False)
 
-train_features_batch, train_labels_batch = next(iter(train_dataloader))
+train_features_batch, train_labels_batch = next(
+    iter(train_dataloader))
 
 
 class_names = train_data.classes
@@ -68,7 +72,7 @@ class_names = train_data.classes
 image, label = train_data[0]
 plt.imshow(image.squeeze(), cmap="gray")
 plt.title(label=class_names[label])
-plt.show()
+# plt.show()
 torch.manual_seed(42)
 fig = plt.figure(figsize=(9, 9))
 rows, cols = 4, 4
@@ -79,7 +83,7 @@ for i in range(1, rows * cols + 1):
     plt.imshow(img.squeeze(), cmap="gray")
     plt.title(class_names[label])
     plt.axis(False)
-plt.show()
+# plt.show()
 
 # prepare dataloader
 
@@ -99,6 +103,30 @@ class FashionMNISTModelV0(nn.Module):
         return self.layer_stack(x)
 
 
+def print_train_time(start: float, end: float, device: torch.device = None):
+    total_time = end - start
+    print(f"Train time on {device}: {total_time} seconds")
+    return total_time
+
+
+def eval_model(model: torch.nn.Module, data_loader: DataLoader,
+               loss_fn: torch.nn.Module, accuracy_fn):
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            y_pred = model(X)
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, y_pred=y_pred.argmax(dim=1))
+        loss /= len(data_loader)
+        acc /= len(data_loader)
+    return {
+        "model_name": model.__class__.__name__,
+        "model_loss": loss.item(),
+        "model_acc": acc
+    }
+
+
 if __name__ == "__main__":
     torch.manual_seed(42)
     model_0 = FashionMNISTModelV0(
@@ -107,6 +135,44 @@ if __name__ == "__main__":
         output_shape=len(class_names)  # one for each class
     ).to(device=device)
     # setup loss, optim, eval metric
-    loss_fn = nn.CrossEntropyLoss
+    loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
-    print('e')
+    trainTimeStart = timer()
+    epochs = 3
+
+    for epoch in tqdm(range(epochs)):
+        print(f"Epoch: {epoch}")
+        train_loss = 0
+
+        for batch, (X, y) in enumerate(train_dataloader):
+            model_0.train()
+            y_pred = model_0(X)
+            loss = loss_fn(y_pred, y)
+            train_loss += loss
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if batch % 400 == 0:
+                print(
+                    f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
+        #
+        train_loss /= len(train_dataloader)
+        test_loss, test_acc = 0.0, 0.0
+        model_0.eval()
+        with torch.inference_mode():
+            for X, y in test_dataloader:
+                test_pred = model_0(X)
+                test_loss += loss_fn(test_pred, y)
+                test_acc += accuracy_fn(
+                    y_true=y, y_pred=test_pred.argmax(dim=1))
+            test_loss /= len(test_dataloader)
+            test_acc /= len(test_dataloader)
+        print(
+            f"\nTrain loss: {train_loss} | Test loss: {test_loss}, test acc {test_acc}"
+        )
+    trainEndTime = timer()
+    totalTraintime = print_train_time(
+        start=trainTimeStart, end=trainEndTime, device=str(next(model_0.parameters()).device))
+    model_0_res = eval_model(
+        model=model_0, data_loader=test_dataloader, loss_fn=loss_fn, accuracy_fn=accuracy_fn)
+    print(model_0_res)

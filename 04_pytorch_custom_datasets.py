ffd892d6ed0ec6dff1bafad50bd7df4a4e76f1bc
Nathan Moes <nathanmoes7@gmail.com>
2023-06-05 10:39:39 -0700

    plot transformed data

--- 04_pytorch_custom_datasets.py
+++ 04_pytorch_custom_datasets.py
@@ -6,6 +6,10 @@ from pathlib import Path
 import os
 import random
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 
 random.seed(42)
@@ -63,5 +67,48 @@ img = Image.open(rnd_image_path)
 print(f"random image path: {rnd_image_path} | and class name: {image_class}")
 print(img)
 
+img_as_array = np.asarray(img)
+plt.figure(figsize=(10, 7))
+plt.imshow(img_as_array)
+plt.title(f"Image class {image_class} | Image shape: {img_as_array.shape}")
+plt.axis(False)
+plt.show()
+
+IMAGE_HEIGHT = 64
+IMAGE_WIDTH = 64
+
+data_transform = transforms.Compose([
+    # resize to 64 x 64
+    transforms.Resize(size=(IMAGE_HEIGHT, IMAGE_WIDTH)),
+    # flip horizontal
+    transforms.RandomHorizontalFlip(p=0.5),
+    # turn image into torch tensor
+    transforms.ToTensor()
+])
+
+data_transform(img)
+
+
+def plot_transformed_images(image_paths: list, transform, n=3, seed=None):
+    if seed:
+        random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(nrows=1, ncols=2)
+            ax[0].imshow(f)
+            ax[0].set_title(f"Original\nSize: {f.size}")
+            ax[0].axis(False)
+            # transformed
+            transformed_img = transform(f).permute(1, 2, 0)  # C,H,W to H,W,C
+            ax[1].imshow(transformed_img)
+            ax[1].set_title(f"Transformed\nSize: {transformed_img.shape}")
+            ax[1].axis(False)
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+    plt.show()
+
+
 if __name__ == "__main__":
+    plot_transformed_images(image_paths=image_path_list,
+                            transform=data_transform, n=3, seed=42)
     print("E")

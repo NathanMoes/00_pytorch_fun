0e8ad212e4d3373bffb6808b508bd7434072f636
Nathan Moes <nathanmoes7@gmail.com>
2023-06-13 18:07:29 -0700

    full grid visualization

--- 08_paper_replication/08_vit_replication.py
+++ 08_paper_replication/08_vit_replication.py
@@ -487,6 +487,47 @@ def visualize():
         axs[i].set_xticks([])
         axs[i].set_yticks([])
     plt.show()
+    # Setup hyperparameters and make sure img_size and patch_size are compatible
+    img_size = 224
+    patch_size = 16
+    num_patches = img_size/patch_size
+    assert img_size % patch_size == 0, "Image size must be divisible by patch size"
+    print(f"Number of patches per row: {num_patches}\
+            \nNumber of patches per column: {num_patches}\
+            \nTotal patches: {num_patches*num_patches}\
+            \nPatch size: {patch_size} pixels x {patch_size} pixels")
+
+    # Create a series of subplots
+    fig, axs = plt.subplots(nrows=img_size // patch_size,  # need int not float
+                            ncols=img_size // patch_size,
+                            figsize=(num_patches, num_patches),
+                            sharex=True,
+                            sharey=True)
+
+    # Loop through height and width of image
+    # iterate through height
+    for i, patch_height in enumerate(range(0, img_size, patch_size)):
+        # iterate through width
+        for j, patch_width in enumerate(range(0, img_size, patch_size)):
+
+            # Plot the permuted image patch (image_permuted -> (Height, Width, Color Channels))
+            axs[i, j].imshow(image_permuted[patch_height:patch_height+patch_size,  # iterate through height
+                                            patch_width:patch_width+patch_size,  # iterate through width
+                                            :])  # get all color channels
+
+            # Set up label information, remove the ticks for clarity and set labels to outside
+            axs[i, j].set_ylabel(i+1,
+                                 rotation="horizontal",
+                                 horizontalalignment="right",
+                                 verticalalignment="center")
+            axs[i, j].set_xlabel(j+1)
+            axs[i, j].set_xticks([])
+            axs[i, j].set_yticks([])
+            axs[i, j].label_outer()
+
+    # Set a super title
+    fig.suptitle(f"{class_names[label]} -> Patchified", fontsize=16)
+    plt.show()
 
 
 # Download pizza, steak, sushi images from GitHub
@@ -529,4 +570,5 @@ embedding_layer_output_shape = (num_patches, patch_size**2 * color_channels)
 
 
 if __name__ == "__main__":
+    visualize()
     pass
